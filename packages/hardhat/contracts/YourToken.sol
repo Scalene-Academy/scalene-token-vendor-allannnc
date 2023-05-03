c5ee0f35ace0cc341a04f7ce24fdbc33f9fa4fa0
allannnc <allan@gmistudios.com.au>
2023-05-03 20:47:46 +1000

    inherit erc20!

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.19; //Do not change the solidity version as it negativly imp
 
 // learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
 // @TODO: YourToken should inherit ERC20
-// import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // @TODO: Implement the YourToken smart contract
-contract YourToken {
+contract YourToken is ERC20 {
 
 }
